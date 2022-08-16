586cdf188fe55e529b523523d822d0b98d754fb2
Chasity2019 <55420918+Chasity2019@users.noreply.github.com>
2022-08-16 15:13:19 -0400

    Update HelloNum.java

--- HelloNum.java
+++ HelloNum.java
@@ -1,5 +1,8 @@
 public class HelloNum{
     public static void main(String[] args){
-        System.out.println("1 2 3 4 5 6 7 8 9 10");
+        System.out.println("Print numbers 1 - 20");
+        for (int i=1; i<=20; i++){
+            System.out.println(i);
+        }
     }
 }
